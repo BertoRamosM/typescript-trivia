e74ce715dc19f33b97bee17013d7a38453f54cac
ARamos <albertramos902@gmail.com>
2024-05-05 10:44:20 +0100

    created main states for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react"
 import QuestionCard from "./components/QuestionCard"
 
+const TOTAL_QUESTIONS = 10;
+
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswer, setUserAnswer] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
 
   const StartTrivia = async() =>{
     
@@ -23,7 +32,14 @@ function App() {
       <p className="score">Score:</p>
       <p>Loading Questions ... </p>
 
-      <QuestionCard />
+      <QuestionCard
+        questionNumber={number +1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={CheckAnswer}
+      />
       <button className="next" onClick={NextQuestion}>Next Question</button>
     </div>
   )
