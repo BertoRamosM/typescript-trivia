c7b440099308462bdb6067e7c62fad4228646f23
ARamos <albertramos902@gmail.com>
2024-05-05 10:24:31 +0100

    typing props for the question cards

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,13 @@
-const QuestionCard = () => {
+type Props = {
+  question: string;
+  asnwer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = () => {
   return (
     <div>QuestionCard</div>
   )
