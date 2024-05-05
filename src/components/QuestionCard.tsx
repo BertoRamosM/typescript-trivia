e4a7b09143d01832f2131537c6e7b10f02ed5bbb
ARamos <albertramos902@gmail.com>
2024-05-05 10:37:47 +0100

    question card created and typed

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,16 +1,37 @@
 type Props = {
   question: string;
-  asnwer: string[];
+  answers: string[];
   callback: any;
-  userAnswer: string;
+  userAnswer: any;
   questionNumber: number;
   totalQuestions: number;
-}
+};
 
-const QuestionCard: React.FC<Props> = () => {
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
   return (
-    <div>QuestionCard</div>
-  )
-}
+    <div>
+      <p className="number">
+        Question: {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{__html: answer}} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default QuestionCard
